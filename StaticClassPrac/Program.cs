0948744a7560f774bc7622d0a1d7a4a1a7ddf17a
James Padgett <jampad14@gmail.com>
2019-01-23 21:08:53 -0500

    Add project files.

---
+++ StaticClassPrac/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassPrac
+{
+    class Program
+    {
+        public void Main(string[] args)
+        {
+            Rollercoaster.NumberOfRuns = 10;
+            Rollercoaster.Speed();
+            Rollercoaster chang = new Rollercoaster();
+        }
+    }
+}
---
+++ StaticClassPrac/Rollercoaster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassPrac
+{
+    public static class Rollercoaster
+    { 
+        public static int NumberOfRuns { get; set; }
+
+        public static void Speed()
+        {
+            Console.WriteLine("How fast are we going??");
+        }
+    }
+}
